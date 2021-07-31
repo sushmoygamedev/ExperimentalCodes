8774ad4a94d5fddf870e81978326d0494799c6fd
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-01 05:09:58 +0600

    AUGUST 01 2021 05:09

---
+++ ExperimentalCodes/ExperimentingPalindrom.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExperimentalCodes
+{
+    public static class ExperimentingPalindrom
+    {
+        public static void PalindromTest()
+        {
+            string str = "kayak";
+            char[] strChar = str.ToCharArray();
+            Array.Reverse(strChar);
+            string strRev = new string(strChar);
+            Console.WriteLine(strRev);
+
+            if (str == strRev)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            /*for (int i = str.Length; i > 0 ; i--)
+            {
+                strRev += (str[i-1]);
+            }
+            if (str == strRev)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }*/
+        }
+    }
+}
---
+++ ExperimentalCodes/ExperimentingRemoveDuplicates1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ExperimentalCodes
+{
+    public static class ExperimentingRemoveDuplicates1
+    {
+        public static void GeeksOfGeeks()
+        {
+            int[] numList = new int[] { 1, 2, 2, 3, 3, 4, 4, 4, 5 };
+            //int[] temp = new int[numList.Length];
+            int j = 0;
+            int[] newNumLIst = new int[numList.Length];
+
+            for (int i = 0; i < numList.Length - 1; i++)
+            {
+                if (numList[i] != numList[i + 1])
+                {
+                    newNumLIst[j] = numList[i];
+                    j++;
+                }
+            }
+            newNumLIst[j] = numList[numList.Length - 1];
+
+            for (j = 0; j < newNumLIst.Length; j++)
+            {
+                Console.WriteLine(newNumLIst[j]);
+            }
+
+            /*for (int i = 0; i < numList.Length-1; i++)
+            {
+                if (numList[i] != numList[i+1])
+                {
+                    temp[j] = numList[i];
+                    j++;
+                }
+            }
+            temp[j] = numList[numList.Length - 1];
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                Console.WriteLine(temp[i]);
+            }*/
+
+            /* for (int i = 0; i < numList.Length-1; i++)
+             {
+                 if (numList[i] != numList[i+1])
+                 {
+                     numList[j] = numList[i];
+                     j++;
+                 }
+             }
+             numList[j] = numList[numList.Length - 1];*/
+        }
+    }
+}
