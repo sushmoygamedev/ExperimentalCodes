f59292ac8fd64b1bce3893cdd8d17d32eaf5a32a
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-03 17:44:55 +0600

    AUGUST 03 2021 17:45

---
+++ ExperimentalCodes/ExperimentRemoveCategoryFromSubreddit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ExperimentalCodes
+{
+    public class ExperimentRemoveCategoryFromSubreddit
+    {
+        public static void CategorySubtract()
+        {
+            string str = "https://www.reddit.com/r/relationships/";
+            /*string[] strSplit = str.Split('/');
+            Console.WriteLine(strSplit[strSplit.Length - 2]);*/
+            string newStr = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '/' || str[i] == '.' || str[i] == ':')
+                {
+                    newStr += " ";
+                }
+                else
+                {
+                    newStr += str[i];
+                }
+            }
+            string[] strSplit = newStr.Split(' ');
+            Console.WriteLine(strSplit[strSplit.Length - 2]);
+        }
+    }
+}
