9f0942da5f0048cad39058834fee854645cbe104
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-14 00:42:44 +0600

    Change Log: August 14 2021 00:42

--- ExperimentalCodes/Program.cs
+++ ExperimentalCodes/Program.cs
@@ -6,7 +6,28 @@ namespace ExperimentalCodes
     {
         static void Main(string[] args)
         {
-            
+            /*string str = "https://www.reddit.com/r/funny/";
+            string[] strSplit = str.Split('/');
+            Console.WriteLine(strSplit[strSplit.Length - 2]);*/
+
+            /*string str = "590O1";
+            string newStr = string.Empty;
+            bool isValid = false;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]) || char.IsWhiteSpace(str[i]))
+                {
+                    newStr += str[i];
+                }
+            }
+            if (newStr.Length == 5)
+            {
+                isValid = true;
+            }
+
+            Console.WriteLine(newStr.Length);
+            Console.WriteLine(isValid);
+            Console.WriteLine(newStr);*/
         }
     }
 }
