d569302a0e866609faa4ff9252c4a6942e3908a0
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-14 18:03:33 +0600

    Change Log: 7 Problem Solve AUGUST 14 2021 18:03

--- ExperimentalCodes/Program.cs
+++ ExperimentalCodes/Program.cs
@@ -28,6 +28,97 @@ namespace ExperimentalCodes
             Console.WriteLine(newStr.Length);
             Console.WriteLine(isValid);
             Console.WriteLine(newStr);*/
+
+            /*string str = "R!=:~0o0./c&}9k`60=y";
+            string newStr = string.Empty;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]))
+                {
+                    newStr += str[i];
+                }
+            }
+            Console.WriteLine(newStr);*/
+
+            /*string str = "I have never seen a thin person drinking Diet Coke.";
+            //char[] vowel = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            string newStr = null;
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case 'a' :
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        newStr += "";
+                        break;
+                    default:
+                        newStr += str[i];
+                        break;
+                }
+            }
+            Console.WriteLine(newStr);*/
+
+            /*string str = "eQuINoX";
+            int[] upperIndex = new int[str.Length];
+            int j = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    upperIndex[j] = i;
+                    j++;
+                }
+            }
+            for (int i = 0; i < j; i++)
+            {
+                if (upperIndex[i] == upperIndex.Length - 1)
+                {
+
+                    Console.Write(upperIndex[i]);
+                }
+                else
+                {
+                    Console.Write(upperIndex[i] + ", ");
+                    
+                }
+            }*/
+
+            /*string str = "Happy Birthday";
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                {
+                    Console.Write(char.ToLower(str[i]));
+                }
+                Console.Write(char.ToUpper(str[i]));
+            }*/
+
+            /*string str = "There is a bomb.";
+            string[] strSplit = str.Split(' ');
+            Console.WriteLine(strSplit[strSplit.Length - 1].Trim('.'));
+
+
+            for (int i = 0; i < strSplit.Length; i++)
+            {
+                if (strSplit[i].Trim('.') == "bomb")
+                {
+                    Console.WriteLine(true);
+                }
+                Console.WriteLine(false);
+            }*/
+
+            /*if (str.Contains("bomb"))
+            {
+                Console.WriteLine("Duck!!!");
+            }
+            else
+            {
+                Console.WriteLine("There is no bomb, relax!");
+            }*/
+            
         }
     }
 }
