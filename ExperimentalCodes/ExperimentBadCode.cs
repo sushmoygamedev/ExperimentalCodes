1b0b43349922e9fa48c54496845f5a93d5bec8c8
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-16 06:56:34 +0600

    Change Log: Experiment Bad Code. AUG 16 2021 06:57

---
+++ ExperimentalCodes/ExperimentBadCode.cs
@@ -0,0 +1,40 @@
+﻿namespace ExperimentalCodes
+{
+    public class ExperimentBadCode
+    {
+        public void ZipValidOrNot()
+        {
+            /*for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    zipCode += str[i];
+                }
+                else if (str.Length > 5 && char.IsLetter(str[i]))
+                {
+                    zipCode += str[i];
+                }
+                else if (str[i] == ' ')
+                {
+                    zipCode += ' ';
+                }
+                else
+                {
+                    zipCode += str[i];
+                }
+            }
+
+            for (int i = 0; i < zipCode.Length; i++)
+            {
+                if (zipCode.Length == 5 && zipCode[i] != ' ' && char.IsDigit(zipCode[i]))
+                {
+                    isValid = true;
+                }
+            }*/
+
+            /*Console.WriteLine("Zip Code: " + zipCode);
+            Console.WriteLine("Zip Code Length: " + zipCode.Length);
+            Console.WriteLine("Valid?: " + isValid);*/
+        }
+    }
+}
