ce7658769396a4cd9caa3f1a48eae7fef5e5014a
sushmoygamedev <sushmoygamedev@gmail.com>
2021-08-26 06:08:59 +0600

    Change Log: Empty program.cs AUG 26 2021 06:08

--- ExperimentalCodes/Program.cs
+++ ExperimentalCodes/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ExperimentalCodes
+﻿using System;
+
+namespace ExperimentalCodes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            // new experiment will coming soon
         }
     }
 }
